202a6a6d59242fad5f54af88519af3da75e21f02
Maxim <kolmax94@gmail.com>
2011-01-09 17:35:07 +0300

    [GUI] Start making source editor

---
+++ source_editor.py
@@ -0,0 +1,32 @@
+
+import gtk, gobject, gtksourceview2
+
+class SourceEditor():
+    """
+    Class representing source editor
+    """
+
+    def __init__(self, widgets_tree):
+        """
+        Constructor for SourceEditor class
+
+        params:
+            - widgets_tree: gtk.glade.XML object with needed widgets
+        """
+
+        #Get main widgets container
+        self.editor_vbox = widgets_tree.get_widget('editor')
+        #Detach it from useless gtk.Window
+        wnd = widget_tree.get_widget('editor_window')
+        wnd.remove(self.editor_vbox)
+        wnd.destroy()
+
+        #Get gtk.ScrolledWindow, which is container for sourceview
+        self.text_area = widgets_tree.get_widget('text_area')
+
+    def get_content(self):
+        """
+        Return main widget for embedding in main window
+        """
+
+        return self.editor_vbox
