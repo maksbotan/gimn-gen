38429d645481b13914eb624107a17d71610abfdf
Maxim <kolmax94@gmail.com>
2011-01-09 20:10:05 +0300

    [GUI] Implement multiply buffers mechanism in editor

--- source_editor.py
+++ source_editor.py
@@ -24,6 +24,9 @@ class SourceEditor():
         #Get gtk.ScrolledWindow, which is container for sourceview
         self.text_area = widgets_tree.get_widget('text_area')
 
+        #Dict of TextBuffers for editor
+        self.buffers = {}
+
         #Initialize source language for SourceView
         self.get_sourcelanguage()
         #Initialize SourceView widget and add to container
@@ -32,12 +35,62 @@ class SourceEditor():
         self.view.set_show_line_numbers(True)
         self.text_area.add(self.view)
 
+        #Create empty buffer and switch to it
+        self.allocate_buffer('empty')
+        self.switch_to_buffer('empty')
+
+    def allocate_buffer(self, name):
+        """
+        Create a new gtk.SourceBuffer, set it's properties and store for future use
+
+        params:
+            - name: Name of buffer to create
+        """
+
+        if name in self.buffers:
+            #Buffer already exists, abort
+            return
+
+        buffer = gtksourceview2.Buffer()
+        buffer.set_language(self.lang)
+
+        self.buffers[name] = buffer
+
+    def remove_buffer(self, name):
+        """
+        Remove no more needed buffer
+
+        params:
+            - name: Name of buffer to remove
+        """
+
+        if not name in self.buffers:
+            #No such buffer exists, abort
+            return
+
+        del self.buffers[name]
+
+    def switch_to_buffer(self, name):
+        """
+        Change current buffer in SourceView to name
+
+        params:
+            - name: Name of buffer to switch to
+        """
+
+        if not name in self.buffers:
+            #No such buffer exists, abort
+            return
+
+        #Do switch
+        self.view.set_buffer(self.buffers[name])
+
     def get_sourcelanguage(self):
         """
         Get gtksourceview2.Language instance for html
         """
-        lm = gtksourceview2.LanguageManager()
-        self.lang = lm.get_language('html')
+        self.lm = gtksourceview2.LanguageManager()
+        self.lang = self.lm.get_language('html')
 
     def get_content(self):
         """
