10c9fffeb590d2562dc803a1d8a74d6f6baf9ebf
Maxim <kolmax94@gmail.com>
2011-02-11 18:39:12 +0300

    [GUI] Make WordImporter handle images

--- word_importer.py
+++ word_importer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/end python
 
-import beautifulsoup, subprocess, tempfile
+import os, subprocess, tempfile, shutil, mmap
+from BeautifulSoup import BeautifulSoup
 
 class WordImporter():
     """
@@ -8,26 +9,34 @@ class WordImporter():
     Uses 'wv' toolkit, http://wvware.sourceforge.net
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename, image_path, image_dest):
         """
         Constructor
 
         params:
             - filename: Name of word file to import from
+            - image_path: Path to images directory (from html point of view)
+            - image_dest: Directory to move images to (in real filesystem)
         """
 
-        if not os.exists(filename):
+        if not os.path.exists(filename):
             raise ValueError('Not existing file given: %s' % filename)
 
         self.file = filename
+        self.image_path = image_path
+        self.image_dest = image_dest
 
     def __del__(self):
         """
         Destructor to ensure temp files are closed and removed from fs
         """
         
-        self.html_map.close()
-        self.html.close()
+        try:
+            self.html_map.close()
+            self.html.close()
+        except AttributeError:
+            return
+
         os.remove(self.html.name)
 
     def get_content(self):
@@ -35,6 +44,34 @@ class WordImporter():
 
     def get_full(self):
         pass
+
+    def _fix_images(self):
+        """
+            Fix paths to images in html
+        """
+
+        #Parse html to BeautifulSoup object
+        self.html_map.seek(0)
+        bs = BeautifulSoup(self.html_map.read(len(self.html_map)))
+
+        #Path to directory where images are stored by wvHtml
+        img_dir = os.path.dirname(self.html.name)
+
+        for image in bs('img'):
+            file_name = image['src']
+
+            #Put image to proper location
+            shutil.move(os.path.join(img_dir, file_name), os.path.join(self.image_dest))
+            #And change img src to proper path
+            image['src'] = '{0}/{1}'.format(self.image_path, file_name)
+
+        #Save fixed html in file
+        html = bs.prettify()
+        print self.html.name
+        self.html_map.seek(0)
+        self.html_map.resize(len(html))
+        self.html_map.write(html)
+        self.html_map.flush()
 
     def _load(self, do_crop=False):
         """
@@ -45,20 +82,26 @@ class WordImporter():
         """
 
         #Get temporary file where wvHtml will store output
-        out_file = tempfile.mkstemp()
+        out_file = tempfile.mkstemp()[1]
 
         #Call wvHtml
-        subprocess.check_call(['wvHtml', self.filename, out_file])
+        subprocess.check_call(['wvHtml', self.file, out_file])
 
         if do_crop:
             #Create mmap object for file
             self.html = open(out_file, 'r+b')
-            self.html_map = mmap.mmap(self.html.fileno())
+            self.html_map = mmap.mmap(self.html.fileno(), 0)
             #Get index of real data section start and end
             #21 is length of header
             start = self.html_map.find('<!--Section Begins-->') + 21
             end = self.html_map.rfind('<!--Section Ends-->')
             #Resize map to new size
-            self.html_map.move(0, start, end-start)
-            self.html_map.resize(end-start)
+            self.html_map.move(0, start, end - start)
+            self.html_map.resize(end - start)
+        else:
+            #Just load output
+            self.html = open(out_file, 'r+b')
+            self.html_map = mmap.mmap(self.html.fileno(), 0)
 
+        #Fix paths to images
+        self._fix_images()
